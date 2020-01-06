63f264501986b847661effa3803eed8e68833924
Wi Su Choi <wisuchoi@gmail.com>
2020-01-06 13:55:43 +0800

    Update About
    Update About

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,21 +26,23 @@ class About extends React.Component {
                         </header>
                         <p>
                             I am currently pursuing my Bachelor's in Computer Science at the National University of Singapore.
-                            My studies are focused on AI, Machine Learning and Deep Learning. While I do find the theroretical 
-                            foundations and logic beneath the subject interesting, I very much prefer to be building something 
-                            which I, and hopefully many others can use.
+                            My studies are focused on AI, Machine Learning and Deep Learning. I also have some interest in cloud 
+                            computing, quantum computing and robotics. While I do find the theroretical foundations and logic 
+                            beneath the subject interesting, I prefer to be building solutions which I, and hopefully many others 
+                            out there, can find useful.
                         </p>
                         <p>
-                            My career goal is to be a full stack developer, in the machine learning domain, to get machine learning 
-                            models deployed to a wider audience to solve some problems or satisfy unmet needs.
+                            My career goal is to be a full stack developer involved in some of the domains listed above, and 
+                            eventually a software architect.
                         </p>
                         <p>
-                            When I am not coding or grinding through academics, I spend my time mainly on the following:
+                            During my free time, I spend my time mainly on the following:
                             <ul>
                                 <li>
                                     <strong>Music</strong>: Beyond Grade 8 piano, violin and theory, I attended some 
                                     higher-level theory classes in the Yong Siew Toh Conservatory on Bach Suites, Romantic Styles 
-                                    and 20th-century music.
+                                    and 20th-century music. I have some interest in how machine learning can be applied to the 
+                                    composition and analytical process.
                                 </li>
                                 <li>
                                     <strong>German</strong>: My goal is to eventually be able to sit through Wagner's <i>Der 
